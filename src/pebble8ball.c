dea22b967abc2de63f764b31d4d2f6cd862c0733
David Ryan <ptcryan@gmail.com>
2015-11-23 21:01:01 -0500

    Migrated to 3.x

--- src/pebble8ball.c
+++ src/pebble8ball.c
@@ -2,7 +2,10 @@
 
 Window *window;
 TextLayer *text_layer;
-InverterLayer *inverter_layer;
+//InverterLayer *inverter_layer;
+bool question_asked = false;
+
+void ask_question(void);
 
 const char *answers[20] = {
   "It is certain",
@@ -30,26 +33,34 @@ const char *answers[20] = {
 int response = 0;
 
 void handle_tap(AccelAxisType axis, int32_t direction) {
-  layer_add_child(window_get_root_layer(window), inverter_layer_get_layer(inverter_layer));
+  if (true == question_asked) {
+    //layer_add_child(window_get_root_layer(window), inverter_layer_get_layer(inverter_layer));
 
-  // tap occured. start 8-ball
-  response = (int)(rand() % 20);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Random number generated: %d", response);
-  text_layer_set_text(text_layer, answers[response]);
+    // tap occured. start 8-ball
+    response = (int)(rand() % 20);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Random number generated: %d", response);
+    text_layer_set_text_color(text_layer, GColorWhite);
+    text_layer_set_background_color(text_layer, GColorBlack);
+    text_layer_set_text(text_layer, answers[response]);
+    question_asked = false;
+  } else {
+    ask_question();
+  }
 }
 
 void ask_question(void) {
   // Set the text, font, and text alignment
   text_layer_set_text(text_layer, "Ask your question and tap your pebble.");
   text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);  
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  question_asked = 1;
 }
 
 void handle_init(void) {
   // Create a window and text layer
   window = window_create();
   text_layer = text_layer_create(GRect(0, 0, 144, 154));
-  inverter_layer = inverter_layer_create(GRect(0, 0, 144, 154));
+  //inverter_layer = inverter_layer_create(GRect(0, 0, 144, 154));
   
   // seed the random number generator
   srand(time(NULL));
@@ -72,7 +83,7 @@ void handle_deinit(void) {
   
   // Destroy the text layer
   text_layer_destroy(text_layer);
-  inverter_layer_destroy(inverter_layer);
+  //inverter_layer_destroy(inverter_layer);
   
   // Destroy the window
   window_destroy(window);
